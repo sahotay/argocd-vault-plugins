b712d78710f27cf37545fcfc822145e8a8422a3d
Rahul Sahotay <12233395+sahotay@users.noreply.github.com>
2024-12-29 23:24:58 +0000

    sample deployment

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -21,4 +21,5 @@ rm -f k9s_linux_amd64.deb
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
+sudo chmod +x /usr/local/bin/helm
 rm -f get_helm.sh
